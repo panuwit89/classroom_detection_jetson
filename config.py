b93b4ad280f353381add818393b1916401495e85
panuwit-nano <panuwit.sun@ku.th>
2025-11-28 16:23:40 +0900

    add device id, mqtt broker ip, mqtt port

--- config.py
+++ config.py
@@ -12,3 +12,7 @@ TRACKER_CONFIG_PATH = "bytetrack_custom.yaml" # Custom Bytetrack Setting
 DETECTOR_CONF_THRESHOLD = 0.1 # The Minimum conf sending to Bytetrack model
 PATIENCE_SECONDS = 3      # Keep object id for this patience time
 SMOOTHING_FACTOR = 0.05    # for Exponential Moving Average (EMA)
+
+DEVICE_ID = "room_101" # แต่ละเครื่องต้องตั้งชื่อไม่ซ้ำกัน
+MQTT_BROKER_IP = "10.32.49.248" # IP ของเครื่อง Server กลาง
+MQTT_PORT = 1883
